201620b58315c2159502992772df42cea9562064
Marina8989 <marina.mill209@gmail.com>
2021-04-04 20:09:26 -0700

    added all js to project

--- main.js
+++ main.js
@@ -80,3 +80,72 @@ const menu = [
         desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed`
     }
 ]
+
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('.btn-container');
+
+//display all items when page loads
+window.addEventListener('DOMContentLoaded', function() {
+    displayMenuItems(menu);
+    displayMenuButtons();
+})
+
+function displayMenuItems(menuItems){
+   let displayMenu = menuItems.map(function (item) {
+       return  `
+       <article class='menu-item'>
+          <img src=${item.img} alt= ${item.title} class='photo'/>
+          <div class='item-info'>
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class='price'>$${item.price}</h4>
+            </header>
+            <p class='item-text'>
+              ${item.desc}
+            </p>
+          </div>
+       </article>`
+   });
+
+   displayMenu = displayMenu.join('');
+
+   sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+    const categories = menu.reduce(
+        (values, item) => {
+            if(!values.includes(item.category)){
+               values.push(item.category);
+            }
+            return values;
+        },
+        ['all']
+    );
+
+    const categoryBtns = categories.map(category => {
+        return `<button type='button' class='filter-btn' data-id=${category}>${category}</button>`
+    }).join('');
+
+   btnContainer.innerHTML = categoryBtns;
+
+   const filterBtns = btnContainer.querySelectorAll('.filter-btn');
+
+   filterBtns.forEach(btn => {
+       btn.addEventListener('click', e => {
+            const category = e.currentTarget.dataset.id;
+           const menuCategory = menu.filter(menuItem => {
+               if(menuItem.category === category) {
+                   return menuItem;
+               }
+           });
+           if(category === 'all'){
+               displayMenuItems(menu);
+           }else {
+               displayMenuItems(menuCategory);
+           }
+       });
+   });
+
+
+}
